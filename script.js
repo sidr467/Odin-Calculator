075b8f480e26eefeca32309544756d0220af803d
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-03-18 14:56:50 +0530

    created some basic calculator opration functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let number1,number2,operator;
+
+function add(){
+    return a + b;
+}
+
+function subtract(){
+    return a + b;
+}
+
+function multiply(){
+    return a * b;
+}
+function divison(){
+    return a / b;
+}
+
+function operate(number1,number2,operator){
+    
+}
